d1e9c6a0dcf2509eb873aa752a897f5d0255378e
Jeremy Kirchhoff <jkirchhoff@intouchhealth.com>
2020-02-24 18:07:13 -0800

    Use ES6 syntax

--- app/javascript/components/Welcome.jsx
+++ app/javascript/components/Welcome.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
 
-const Welcome = props => (
-  <h2 class='welcome'>Welcome to {props.name}!</h2>
-)
+class Welcome extends React.Component {
+  render() {
+    return <h2 className='welcome'>Welcome to {this.props.name}!</h2>;
+  }
+}
 
 Welcome.defaultProps = {
   name: 'default'
-}
-
+};
 export default Welcome
