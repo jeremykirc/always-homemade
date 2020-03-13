48302111ade99a02b2c542d435539d08420e1ee3
Jeremy Kirchhoff <jkirchhoff@intouchhealth.com>
2020-03-12 18:48:48 -0700

    Add different image editor

--- app/javascript/components/ImageUpload.jsx
+++ app/javascript/components/ImageUpload.jsx
@@ -1,41 +1,41 @@
 import React from 'react';
-import { FilePond, registerPlugin } from 'react-filepond';
-import FilePondPluginImageExifOrientation from 'filepond-plugin-image-exif-orientation';
-import FilePondPluginImagePreview from 'filepond-plugin-image-preview';
-import FilePondPluginImageCrop from 'filepond-plugin-image-crop';
-import FilePondPluginImageEdit from 'filepond-plugin-image-edit';
-
-registerPlugin(FilePondPluginImageExifOrientation, FilePondPluginImagePreview, FilePondPluginImageCrop, FilePondPluginImageEdit);
+import ImageEditor from '@toast-ui/react-image-editor';
 
 class ImageUpload extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      files: []
     };
   }
 
-  handleInit = () => {
-    console.log('FilePond instance has initialised', this.pond);
-  }
+  myTheme = {
+    // Theme object to extends default dark theme.
+  };
 
   render() {
     return(
-      <FilePond
-        ref={ref => this.pond = ref}
-        files={this.state.files}
-        allowMultiple={false}
-        allowImageEdit={true}
-	server='/upload'
-	instantUpload={false}
-        imageCropAspectRatio='1:1'
-	oninit={this.handleInit}
-        onupdatefiles={(fileItems) => {
-	  this.setState({
-	    files: fileItems.map(fileItem => fileItem.file)
-	  });
-        }}
-        labelIdle="Drag & Drop your files or <span class='filepond--label-action'>Browse</span>"
+      <ImageEditor
+	includeUI={{
+	  loadImage: {
+	    path: 'img/sampleImage.jpg',
+	    name: 'SampleImage'
+	  },
+	  theme: this.myTheme,
+	  menu: ['crop', 'filter'],
+	  initMenu: 'crop',
+	  uiSize: {
+	    width: '1000px',
+	    height: '700px'
+	  },
+	  menuBarPosition: 'bottom'
+	}}
+	cssMaxHeight={500}
+	cssMaxWidth={700}
+	selectionStyle={{
+	  cornerSize: 20,
+	  rotatingPointOffset: 70
+	}}
+	usageStatistics={true}
       />
     )
   }
