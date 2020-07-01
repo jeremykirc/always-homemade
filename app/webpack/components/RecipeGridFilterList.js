406e8109229f6c2274cb4b68b78d81e0f850bc42
Jeremy Kirchhoff <jkirchhoff@intouchhealth.com>
2020-06-30 17:45:14 -0700

    Clean up UIg

--- app/webpack/components/RecipeGridFilterList.js
+++ app/webpack/components/RecipeGridFilterList.js
@@ -7,14 +7,14 @@ import RecipeGridSearch from './RecipeGridSearch';
 const RecipeGridFilterList = () => {
   return (
     <Row>
-      <Col xs='6' className='recipe-group-filters-container'>
+      <Col sm='6' className='recipe-group-filters-container'>
         <ul>
           <RecipeGridFilter displayText='All' />
-          <RecipeGridFilter displayText='Only Mine' group='OWN' />
+          <RecipeGridFilter displayText='My Recipes' group='OWN' />
           <RecipeGridFilter displayText='Favorites' group='FAVORITES' />
         </ul>
       </Col>
-      <Col xs='6' className='recipe-search-container'>
+      <Col sm='6' className='recipe-search-container'>
         <RecipeGridSearch />
       </Col>
     </Row>
