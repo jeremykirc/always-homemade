8fab70fae9271a61728701b615abb3a75e888860
Kevin Kirchhoff <kevin.kirchhoff@logmein.com>
2020-07-11 15:22:44 -0700

    add a model for ingredient quantity

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_11_220238) do
+ActiveRecord::Schema.define(version: 2020_07_11_220820) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -31,6 +31,17 @@ ActiveRecord::Schema.define(version: 2020_07_11_220238) do
     t.string "checksum", null: false
     t.datetime "created_at", null: false
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
+
+  create_table "ingredient_quantities", force: :cascade do |t|
+    t.integer "recipe_id", null: false
+    t.integer "ingredient_id", null: false
+    t.float "quantity", null: false
+    t.string "unit"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["ingredient_id"], name: "index_ingredient_quantities_on_ingredient_id"
+    t.index ["recipe_id"], name: "index_ingredient_quantities_on_recipe_id"
   end
 
   create_table "ingredients", force: :cascade do |t|
@@ -60,5 +71,7 @@ ActiveRecord::Schema.define(version: 2020_07_11_220238) do
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "ingredient_quantities", "ingredients"
+  add_foreign_key "ingredient_quantities", "recipes"
   add_foreign_key "recipes", "users"
 end
